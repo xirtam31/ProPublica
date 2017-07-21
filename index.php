bf50e088409a857073162a6cd05c896aeba6b92d
Brian Placko <placko@Nichols-iMac.local>
2017-07-21 16:14:24 -0400

    twig update

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 require 'vendor/autoload.php';
 require_once 'ProPublicaApiInfo.php';
 
+$test_filter = new Twig_Filter('test_filt', 'ProPublicaApiInfo::orgInfoGeneral');
 
+$loader = new Twig_Loader_Filesystem('/Users/placko/Web/ProPublica/templates');
+$twig = new Twig_Environment($loader);
+$twig->addFilter($test_filter);
+
+echo $twig->render('twig_test.php');
 
 
 
---
+++ templates/twig_test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+?>
+
+<html>
+    {% set test = 'test 2' %}
+    {{ test }}
+    test
+    
+    
+</html>
